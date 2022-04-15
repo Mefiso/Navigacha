be57d64fc656396e4d6f5fceb7b63bdd83711688
Mefiso <polpegra@gmail.com>
2022-04-15 14:57:55 +0200

    almost finished tool

--- Navigacha/Assets/Helpers.cs
+++ Navigacha/Assets/Helpers.cs
@@ -12,6 +12,50 @@ public enum Class
     SPELLSLINGER
 }
 
+[Serializable]
+public class SerializableList<T>
+{
+    public List<T> list = new List<T>();
+}
+
+[Serializable]
+public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private List<TKey> keys = new List<TKey>();
+
+    [SerializeField]
+    private List<TValue> values = new List<TValue>();
+
+    // save the dictionary to lists
+    public void OnBeforeSerialize()
+    {
+        keys.Clear();
+        values.Clear();
+        foreach (KeyValuePair<TKey, TValue> pair in this)
+        {
+            keys.Add(pair.Key);
+            values.Add(pair.Value);
+        }
+    }
+
+    // load dictionary from lists
+    public void OnAfterDeserialize()
+    {
+        this.Clear();
+
+        if (keys.Count != values.Count)
+            throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
+
+        for (int i = 0; i < keys.Count; i++)
+            this.Add(keys[i], values[i]);
+    }
+}
+
+[Serializable] public class DictStringAndInt : SerializableDictionary<string, int[]> { }
+[Serializable] public class DictVector2AndString : SerializableDictionary<Vector2, string> { }
+
+
 namespace Helpers
 {
     public interface IUnit
@@ -25,12 +69,20 @@ namespace Helpers
         public const int L_BOUNDARY = -3;
         public const int R_BOUNDARY = 4;
         public const int V_BOUNDARY = 6;
+        public const int MAX_ENEMIES = 10;
 
         static public Vector2 Square(int x, int y)
         {
             return new Vector2(L_BOUNDARY + (x + 0.5f) * SQUARE_SIZE,
                            V_BOUNDARY - (y + 0.5f) * SQUARE_SIZE);
         }
+
+        static public Vector2 GetSquareIn(Vector2 position)
+        {
+            return new Vector2(Mathf.Floor((position.x - L_BOUNDARY) / SQUARE_SIZE),
+                                Mathf.Floor((V_BOUNDARY - position.y) / SQUARE_SIZE));
+        }
+
         static public Vector3 PositionToGrid (Vector3 position)
         {
             return new Vector3(Mathf.Floor(position.x) + 0.5f*SQUARE_SIZE,
---
+++ Navigacha/Assets/Map.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[Serializable]
+class Stage
+{
+    public int ID;
+
+    int dungeonID;
+    int nWaves;
+    bool isExit = false;
+    bool isEntrance = false;
+    [SerializeField] DictVector2AndString squares =
+        new DictVector2AndString();
+    [SerializeField] SerializableList<int> enemyIDs = new SerializableList<int>();
+    [SerializeField] SerializableList<int>[] waveSpawnPoints;
+    List<int> connections = new List<int>();
+
+    public void GenerateID()
+    {
+        ID = UnityEngine.Random.Range(1, int.MaxValue);
+    }
+
+    public void SetDungeonID(int dID)
+    {
+        dungeonID = dID;
+    }
+
+    public void SetWavesNumber(int n)
+    {
+        nWaves = n;
+        waveSpawnPoints = new SerializableList<int>[nWaves];
+    }
+
+    public void MakeEntrance()
+    {
+        isEntrance = true;
+    }
+
+    public void MakeExit()
+    {
+        isExit = true;
+    }
+
+    public void AddSquare(Vector2 coords, string type)
+    {
+        squares.Add(coords, type);
+    }
+
+    public void RemoveSquare(Vector2 coords)
+    {
+        squares.Remove(coords);
+    }
+
+    public void ConnectWith(int sID)
+    {
+        connections.Add(sID);
+    }
+
+}
+
+[Serializable]
+class Dungeon
+{
+    public int ID;
+    public string name;
+    [SerializeField] SerializableList<int> stagesID = new SerializableList<int>();
+    int plane = 0;
+    int dungeonType = 0;
+    [SerializeField] DictStringAndInt connections = new DictStringAndInt();
+
+    public void GenerateID()
+    {
+        ID = UnityEngine.Random.Range(1, int.MaxValue);
+    }
+    public void SetPlaneAndDungeonType(int p, int dt)
+    {
+        plane = p;
+        dungeonType = dt;
+    }
+    public void AddStage(Stage newStage)
+    {
+        stagesID.list.Add(newStage.ID);
+    }
+    public void RemoveStage(int id)
+    {
+        stagesID.list.Remove(id);
+    }
+
+    public void GenerateConnection(string name, int firstID, int secondID)
+    {
+        connections.Add(name, new int[2] { firstID, secondID });
+    }
+
+    public void RemoveConnection (string name)
+    {
+        connections.Remove(name);
+    }
+}
