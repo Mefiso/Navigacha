075bbf00a89fd42c13db61ca68e8e57ae72618c1
Mefiso <polpegra@gmail.com>
2022-04-21 19:04:05 +0200

    improved map tool

---
+++ Navigacha/Assets/Scripts/Art.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public enum ArtOrigin
+{
+    CASTER,
+    TARGET,
+    INTERACTABLE,
+    EMPTY
+}
+
+/*
+ * Effect convention
+ * -----------------
+ *
+ * D = damage
+ * H = heal
+ * P = push
+ * A = attract
+ * B1 to B7 = buff
+ * P = orb or hazard
+ * X = destroy obstacle or Trap
+ *
+ */
+
+class Art
+{
+    string name;
+    string description;
+
+    // Shape
+    ArtOrigin origin;
+    bool directional;
+    int range;
+
+    SerializableList<SerializablePair<string, SerializableList<float>>[]> effectMatrix;
+
+    // Power source
+    float[] statsDamage = new float[7];
+    float[] statsBuff = new float[7];
+    float[] statsHeal = new float[7];
+
+    void Perform()
+    {
+
+    }
+}
